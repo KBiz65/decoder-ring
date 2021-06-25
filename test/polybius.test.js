cf73ccb9f93df8195fa9888fa5700f7b8689bc33
Kevin Bisner <34324949+KBiz65@users.noreply.github.com>
2021-06-25 10:26:07 -0700

    completed polybius files

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,3 +1,55 @@
 // Write your tests here!
 const { expect } = require("chai");
 const { polybius } = require("../src/polybius");
+
+describe("polybius", () => {
+  it("should return false if the input is empty, missing, or not a string", () => {
+    // actual results that come back from calling polybius
+    const actualEmptyString = polybius("");
+    const actualMissingMessage = polybius();
+    const actualMessageNotAString = polybius(65);
+
+    // expect(actual).to.be.false;
+    expect(actualEmptyString).to.be.false;
+    expect(actualMissingMessage).to.be.false;
+    expect(actualMessageNotAString).to.be.false;
+  });
+
+  it("should return the correct encoded response", () => {
+    // actual results from calling polybius()
+    const actualNoSpace = polybius("Kevin");
+    const actualWithSpace = polybius("Kevin Bisner");
+
+    // setting up the variables with what should be expected from calling polybius
+    const expectedNoSpace = "5251154233";
+    const expectedWithSpace = "5251154233 214234335124";
+
+    // expect(actual).to.be.equal(expected);
+    expect(actualNoSpace).to.be.equal(expectedNoSpace);
+    expect(actualWithSpace).to.be.equal(expectedWithSpace);
+  });
+
+  it("should return false if decoded input has an odd amount of numbers", () => {
+    // actual results from calling polybius()
+    const actualNoSpace = polybius("525115423", false);
+    const actualWithSpace = polybius("5251154233 21423433512", false);
+
+    // expect(actual).to.be.false;
+    expect(actualNoSpace).to.be.false;
+    expect(actualWithSpace).to.be.false;
+  });
+
+  it("should return the correct decoded response", () => {
+    // actual results from calling polybius()
+    const actualNoSpace = polybius("5251154233", false);
+    const actualWithSpace = polybius("5251154233 214234335124", false);
+
+    // setting up the variables with what should be expected from calling polybius
+    const expectedNoSpace = "kev(i/j)n";
+    const expectedWithSpace = "kev(i/j)n b(i/j)sner";
+
+    // expect(actual).to.be.equal(expected);
+    expect(actualNoSpace).to.be.equal(expectedNoSpace);
+    expect(actualWithSpace).to.be.equal(expectedWithSpace);
+  });
+});
